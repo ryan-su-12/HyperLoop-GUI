c19afd1229ec46ef4ecadffe26e48f3742c5c660
hubertstarosta <128559378+hubertstarosta@users.noreply.github.com>
2023-03-25 19:37:24 -0400

    Update hyperloopGui.py

--- hyperloopGui.py
+++ hyperloopGui.py
@@ -1,16 +1,18 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
- 
+form_data = {'Velocity':"Hello",'Acceleration':0,'Time': 0}
 
 @app.route('/')
 def home():
-    return render_template('Home.html')
+    
+    return render_template('Home.html', variable = form_data.get('Velocity'))
 
-@app.route('/Battery/')
-def Battery():
-    return render_template('Battery.html')
+       
+
+@app.route('/Electrical_Subsystem/')
+def Electrical_Subsystem():
+    return form_data
 
 @app.route('/Velocity/')
 def Velocity():
@@ -20,10 +22,21 @@ def Velocity():
 def Temperatures():
     return render_template('Temperatures.html')
 
-@app.route('/System_Status/')
-def System_Status():
-    return render_template('System_Status.html')
+@app.route('/Mechanical_Subsystem/')
+def Mechanical_Subsystem():
+    return render_template('Mechanical_Subsystems.html', variable = form_data.get('Velocity'))
 
+@app.route('/Input_Cases/')
+def Input_Cases():
+    return render_template('Input_Cases.html')
 
-if __name__=='__main__':
-    app.run()
+@app.route('/data/', methods = ['POST'])
+def data():
+    if request.method == 'POST':
+        form_data['Velocity'] = request.form['Velocity']
+        form_data['Time'] = request.form['Time']
+        form_data['Acceleration'] = request.form['Acceleration']
+        return redirect(url_for('home')), form_data
+
+ 
+ 
